c214442252963cbb3addbdc85da1af73df86b024
tushardahiya <tushardahiya84@gmail.com>
2021-03-05 15:57:40 +0530

    setup the initial nextjs project

---
+++ pages/auth/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const authIndexPage = () => (
+    <div>
+        <h1>the auth index page</h1>
+    </div>
+);
+
+export default authIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const indexPage = () => (
+    <div>
+        <h1>the main page</h1>
+    </div>
+);
+
+export default indexPage;
