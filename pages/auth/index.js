fb22ae50afe40fc783b79048c264dfac220f29dc
tushardahiya <tushardahiya84@gmail.com>
2021-03-05 20:21:37 +0530

    tried using components in pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import User from '../../components/User';
 
 const authIndexPage = () => (
     <div>
         <h1>the auth index page</h1>
+        <User name="tushar" age="22" />
     </div>
 );
 
